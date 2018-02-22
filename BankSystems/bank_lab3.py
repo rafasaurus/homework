e322b6737536e8b60d800d11a32304106e90d76a
Rafael <rafa.grigorian@gmail.com>
2018-02-22 11:10:32 +0400

    bank lab3

--- BankSystems/bank_lab3.py
+++ BankSystems/bank_lab3.py
@@ -2,22 +2,27 @@ import numpy as np
 
 arr = np.array([19, 11, 4, 3.5, 3, 1.5, 1], dtype=float)
 p = np.sort(arr,kind='quicksort',order=None,axis=-1)[::-1]
+def statementLessThen(global_i,debug):
+    return arr[global_i] < debug
+def statementGreaterThen(global_i,debug):
+    return arr[global_i] > debug
 
-def f0(global_i):
+def globalStatement(global_i):
     for local_i in range(arr.size,global_i, -1):
         debug = np.sum(arr[local_i:])
         #print(debug)
         #print()
-        if arr[global_i] < debug:
+
+        if statementLessThen(global_i,debug):
             #print("arrf0=", arr[global_i])
             #print("arr_f0_index=", global_i)
             return 1
 
 if p.any() == arr.any():
     for i in range(arr.size-2, -1, -1):
-        f0(i)
+        globalStatement(i)
         print("++++++++++++++")
-        if f0(i) == 1:
+        if globalStatement(i) == 1:
             print("for i=", i)
             print("arr=", arr[i])
 
