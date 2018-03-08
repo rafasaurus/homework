a8a4a2eea053851f068ce4759083aafbc89d2869
Rafael <rafa.grigorian@gmail.com>
2018-03-08 18:28:27 +0400

    full

--- OperationsResearch/reliability_problem.py
+++ OperationsResearch/reliability_problem.py
@@ -24,17 +24,22 @@ def func(index):
     arr = np.array([],dtype=float)
     global m
     global arr_global
+    global C
     index-=1
     print("index",index,"################################################################################")
-   # arr_global = np.append(arr_global) = np.array([],dtype=float)
+    #print("C=",C)
+    #arr_global = np.append(arr_global) = np.array([],dtype=float)
     #print("type of m============",type(m))
     if index == 0:
+        print("int(C/cost[index])+1)",int(C/cost[index])+1)
         for i in range(int(C/cost[index])+1):#C/ci
             arr = np.append(arr, phi(p[index],i)*np.exp(-l*i*weight[index]))
         arr_global = np.append(arr_global,np.max(arr))
         #print(arr)
         #print("reliable quantity of m1 = ",np.argmax(aarr_global = np.append(arr_global)rr,axis = 0))
         m = np.append(m, np.argmax(arr))
+        C -= m[index]*cost[index]
+        print("C=",C)
         dictionary = {"arr_max":np.max(arr),"m":m}
         print("th dictionary=",dictionary,"==========================================================") 
         print("m=============",m) 
@@ -43,12 +48,15 @@ def func(index):
         #print(int(C/cost[index]))
         #return 0
         dictionary = func(index)
+        print("int(C/cost[index])+1)",int(C/cost[index])+1)
         for i in range(int(C/cost[index])+1):#C/c
             arr = np.append(arr, phi(p[index],i)*np.exp(-l*i*weight[index])*dictionary["arr_max"])
         arr_global = np.append(arr_global,np.max(arr)) 
         #print(arr)
         #print("reliable quantity of m1 = ",np.argmax(arr,axis = 0))
         m = np.append(m, np.argmax(arr))
+        C -= m[index]*cost[index]
+        print("C=",C)
         print("th dictionary=",dictionary,"==========================================================")
         print("m=============",m)
         dictionary = {"arr_max":np.max(arr),"m":m}
@@ -60,4 +68,10 @@ print("arr_global = ",arr_global)
 print("--------------")
 print("wjmj",np.dot(weight,mj))
 print("cjmj",np.dot(cost,mj))
-print("answer",np.prod(arr_global))
+answer = 1 # np.multiply(arr_global, np.exp(np.dot(*global_dictionary["m"],weight)))
+
+for i in range(N):
+    answer = answer * phi(p[i],global_dictionary["m"][i]) * np.exp(-l*global_dictionary["m"][i]*weight[i])
+print("answer",answer)
+print("total weight is ", np.dot(weight,global_dictionary["m"]))
+print("total cost is",np.dot(cost,global_dictionary["m"]))
