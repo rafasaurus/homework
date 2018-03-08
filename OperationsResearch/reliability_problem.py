a5e0f00c64bf4611b1b2bb62a36fb6d006deaaa8
Rafael <rafa.grigorian@gmail.com>
2018-03-08 20:58:50 +0400

    time added

--- OperationsResearch/reliability_problem.py
+++ OperationsResearch/reliability_problem.py
@@ -6,13 +6,12 @@ p = np.array([0.9, 0.75, 0.65, 0.8, 0.85], dtype=float)
 cost = np.array([5, 4, 9, 7, 7], dtype=int)
 weight = np.array([8, 9, 6, 7, 8], dtype=int)
 
-l = 0.001
+__lambda__ = 0.001
 
-mj = np.array([2, 3, 4, 2, 2], dtype=int)
 N = 5
 
 
-def phi(p, m):
+def prob(p, m):
     return 1-pow(1-p, 1+m)
 
 
@@ -27,52 +26,43 @@ def func(index):
     global arr_global
     global C
     index -= 1
-    print("index", index, "################################################################################")
     # print("C=",C)
     # arr_global = np.append(arr_global) = np.array([],dtype=float)
     # print("type of m============",type(m))
     if index == 0:
-        print("int(C/cost[index])+1)", int(C/cost[index])+1)
         for i in range(int(C/cost[index])+1): # C/ci
-            arr = np.append(arr, phi(p[index], i)*np.exp(-l*i*weight[index]))
+            arr = np.append(arr, prob(p[index], i)*np.exp(-__lambda__*i*weight[index]))
         arr_global = np.append(arr_global, np.max(arr))
         m = np.append(m, np.argmax(arr))
         C -= m[index]*cost[index]
         print("C=", C)
         dictionary = {"arr_max": np.max(arr), "m": m}
         print("th dictionary=", dictionary, "==========================================================")
-        print("m=============", )
+        #print("m=============", m)
         return dictionary
     else:
-        # print(int(C/cost[index]))
-        # return 0
         dictionary = func(index)
-        print("int(C/cost[index])+1)", int(C/cost[index])+1)
         for i in range(int(C/cost[index])+1):  # C/c
-            arr = np.append(arr, phi(p[index], i)*np.exp(-l*i*weight[index])*dictionary["arr_max"])
+            arr = np.append(arr, prob(p[index], i)*np.exp(-__lambda__*i*weight[index])*dictionary["arr_max"])
         arr_global = np.append(arr_global, np.max(arr)) 
-        # print(arr)
-        # print("reliable quantity of m1 = ",np.argmax(arr,axis = 0))
         m = np.append(m, np.argmax(arr))
         C -= m[index]*cost[index]
         print("C=", C)
-        print("th dictionary=", dictionary, "==========================================================")
-        print("m=============", m)
+        print("th dictionary=", dictionary,) 
+        #print("m=============", m)
         dictionary = {"arr_max": np.max(arr), "m": m}
         return dictionary
 
 start_time = time.time()
 global_dictionary = func(index)
 elapsed_time = time.time()-start_time
-print("global_dict=", global_dictionary["m"])
+print("global_dict=", global_dictionary)
 print("arr_global = ", arr_global)
 print("--------------")
-print("wjmj", np.dot(weight, mj))
-print("cjmj", np.dot(cost, mj))
 answer = 1  # np.multiply(arr_global, np.exp(np.dot(*global_dictionary["m"],weight)))
 
 for i in range(N):
-    answer = answer * phi(p[i],  global_dictionary["m"][i]) * np.exp(-l*global_dictionary["m"][i]*weight[i])
+    answer = answer * prob(p[i],  global_dictionary["m"][i]) * np.exp(-__lambda__*global_dictionary["m"][i]*weight[i])
 print("answer", answer)
 print("total weight is ", np.dot(weight, global_dictionary["m"]))
 print("total cost is", np.dot(cost, global_dictionary["m"]))
