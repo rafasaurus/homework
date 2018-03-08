51916651ff4dc2c6a51c80adb44de6bb1353935d
Rafael <rafa.grigorian@gmail.com>
2018-03-08 17:13:18 +0400

    worked !

--- OperationsResearch/test.py
+++ OperationsResearch/test.py
@@ -8,7 +8,6 @@ weight = np.array([8,9,6,7,8],dtype=int)
 l= 0.001
 
 mj = np.array([2,3,4,2,2],dtype = int)
-print(np.dot(mj,weight))
 N=5
 #def phi(p,mj):
 #    q=1
@@ -18,14 +17,40 @@ N=5
 #print(phi(p,mj))
 def phi(p,m):
     return 1-pow(1-p, 1+m)
-
-def func():
+index = 5
+m = np.array([],dtype=int)
+def func(index):
+    global m
+    index-=1
+    print("index",index,"################################################################################")
     arr = np.array([],dtype=float)
-    for i in range(int(C/cost[0])+1):#C/ci
-        middle = phi(p[0],i)
-        arr = np.append(arr, middle*np.exp(-l*mj[0]*weight[0]))
-    print(arr)
-    print("reliable quantity of m1 = ",np.argmax(arr,axis = 0))
-func()
+    #print("type of m============",type(m))
+    if index == 0:
+        for i in range(int(C/cost[index])+1):#C/ci
+            arr = np.append(arr, phi(p[index],i)*np.exp(-l*i*weight[index]))
+        #print(arr)
+        #print("reliable quantity of m1 = ",np.argmax(arr,axis = 0))
+        m = np.append(m, np.argmax(arr))
+        dictionary = {"arr_max":np.max(arr),"m":m}
+        print("th dictionary=",dictionary,"==========================================================") 
+        print("m=============",m) 
+        return dictionary
+    else :
+        #print(int(C/cost[index]))
+        #return 0
+        dictionary = func(index)
+        for i in range(int(C/cost[index])+1):#C/c
+            arr = np.append(arr, phi(p[index],i)*np.exp(-l*i*weight[index])*dictionary["arr_max"])
+        #print(arr)
+        #print("reliable quantity of m1 = ",np.argmax(arr,axis = 0))
+        m = np.append(m, np.argmax(arr))
+        print("th dictionary=",dictionary,"==========================================================")
+        print("m=============",m)
+        dictionary = {"arr_max":np.max(arr),"m":m}
+        return dictionary
+    
+global_dictionary = func(index)
+print("global_dict=",global_dictionary["m"])
+print("--------------")
 print("wjmj",np.dot(weight,mj))
 print("cjmj",np.dot(cost,mj))
