4b0a93011a2c4d821bb692d4e8128ea0ff75a793
Rafael <rafa.grigorian@gmail.com>
2018-03-14 20:59:49 +0400

    with bugs

--- operationsResearch/my_reliability_problem.py
+++ operationsResearch/my_reliability_problem.py
@@ -1,6 +1,7 @@
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+import copy
 # input data
 p = np.array([0.9, 0.75, 0.65, 0.8, 0.85], dtype=float)
 cost = np.array([5, 4, 9, 7, 7], dtype=int)
@@ -21,9 +22,23 @@ m = np.array([], dtype=int)
 arr_global = np.array([], dtype=int)
 
 
+
 def prob(p, m):
     return 1-pow(1-p, 1+m)
 
+def compute_global_prob(dictionary):
+    answer = 1
+    # calculating the answer
+    print("debug")
+    print(dictionary)
+    for i in range(N):
+        answer = answer * prob(p[i],  dictionary["m"][i]) * np.exp(-dictionary["lambda"]*dictionary["m"][i]*weight[i])
+    wm = np.dot(dictionary["m"], weight) 
+    cm = np.dot(dictionary["m"], cost)
+    #print("m", dictionary["m"])
+    #print("the probability of successful operation is", answer*np.exp(dictionary["lambda"]*np.dot(weight, dictionary["m"]))) 
+    return_dict = {"prob":answer*np.exp(dictionary["lambda"]*np.dot(weight, dictionary["m"])),"wm":wm, "cm":cm, "m":dictionary["m"]}
+    return return_dict 
 
 def func(index):
     arr = np.array([], dtype=float)
@@ -72,7 +87,13 @@ lambda_last = 0
 lambda_current = 0
 lambda_min = 0
 lambda_max = 0
+lambda_min_wm = 0
+lambda_max_wm = 0
+lambda_max_dict = {}
 __lambda__ = 0
+global_wm = 0
+global_dictionary = {}
+lambda_min_dict = {}
 lambda_exceeded_status = False # if true then it exceeded from >0 to <0
 for i in np.arange(0.00001, 0.0005, 0.00001):
     
@@ -85,14 +106,23 @@ for i in np.arange(0.00001, 0.0005, 0.00001):
     m = np.array([], dtype=int)
     arr_global = np.array([], dtype=int)
     C = C_global
+    print("deep debug ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", lambda_min_dict)
     global_dictionary = func(index)
-
+    lambda_min_dict = copy.deepcopy(lambda_max_dict)
+    lambda_max_dict = copy.deepcopy(global_dictionary)
     answer = 1
     # calculating the answer
     for i in range(N):
         answer = answer * prob(p[i],  global_dictionary["m"][i]) * np.exp(-__lambda__*global_dictionary["m"][i]*weight[i])
 
+    lambda_min_wm = global_wm
+
     global_wm = np.dot(global_dictionary["m"],weight)
+
+    lambda_max_wm = global_wm
+
+
+
     print("m", global_dictionary["m"])
     print("wm=", global_wm) 
     print("cm", np.dot(global_dictionary["m"], cost))
@@ -104,21 +134,25 @@ for i in np.arange(0.00001, 0.0005, 0.00001):
         boolean = False
         max_probability = answer*np.exp(__lambda__*np.dot(weight, global_dictionary["m"])) 
         max_lambda = __lambda__
-        max_dictionary = global_dictionary
+        max_dictionary = global_dictionary.copy()
         print()
 
     if (answer*np.exp(__lambda__*np.dot(weight, global_dictionary["m"]))) > max_probability:
         max_lambda = __lambda__
-        max_dictionary = global_dictionary 
+        max_dictionary = global_dictionary.copy()
         max_probability = answer*np.exp(__lambda__*np.dot(weight, global_dictionary["m"])) 
     print("max prob in each iteeration",max_probability)
+    
     wm_array = np.append(wm_array,global_wm)
     lambda_array = np.append(lambda_array,__lambda__)
     lambda_exceeded_status = bool(global_wm - W <= 0) 
 
+
     if lambda_exceeded_status == True:
         lambda_min = lambda_last
+        lambda_min_dict = lambda_min_dict.update({'lambda':lambda_min})
         lambda_max = lambda_current
+        lambda_max_dict = lambda_max_dict.update({'lambda':lambda_max})
         break
         
 elapsed_time = time.time()-start_time
@@ -140,7 +174,9 @@ print("\n\ntime elapsed for the program in ms ", elapsed_time*1000)
 print(lambda_array, wm_array)
 
 print("lambda_current=", lambda_current, "lambda_last=", lambda_last)
-
-plt.scatter(wm_array, lambda_array, s=1)
-plt.show()
-
+print("lambda_min_wm=", lambda_min_wm, "lambda_max_wm", lambda_max_wm)
+# plt.scatter(wm_array, lambda_array, s=1)
+# plt.show()
+print("lambda_min_dict=", lambda_min_dict)
+print(compute_global_prob(lambda_min_dict))
+print(compute_global_prob(lambda_max_dict))
