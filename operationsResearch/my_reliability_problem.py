912596942fdc564b080065b6d5e04c1d00ded82d
Rafael <rafa.grigorian@gmail.com>
2018-03-17 19:35:35 +0400

    cleaned

--- operationsResearch/my_reliability_problem.py
+++ operationsResearch/my_reliability_problem.py
@@ -21,22 +21,17 @@ C_global = C
 m = np.array([], dtype=int)
 arr_global = np.array([], dtype=int)
 
-
-
 def prob(p, m):
     return 1-pow(1-p, 1+m)
 
 def compute_global_prob(dictionary):
     answer = 1
-    # calculating the answer
-    #print("debug")
-    #print(dictionary)
+
     for i in range(N):
         answer = answer * prob(p[i],  dictionary["m"][i]) * np.exp(-dictionary["lambda"]*dictionary["m"][i]*weight[i])
     wm = np.dot(dictionary["m"], weight) 
     cm = np.dot(dictionary["m"], cost)
-    #print("m", dictionary["m"])
-    #print("the probability of successful operation is", answer*np.exp(dictionary["lambda"]*np.dot(weight, dictionary["m"]))) 
+
     return_dict = {"prob":answer*np.exp(dictionary["lambda"]*np.dot(weight, dictionary["m"])),"wm":wm, "cm":cm, "m":dictionary["m"]}
     return return_dict 
 
@@ -46,9 +41,7 @@ def func(index, __lambda__):
     global arr_global
     global C
     index -= 1
-    # print("C=",C)
-    # arr_global = np.append(arr_global) = np.array([],dtype=float)
-    # print("type of m============",type(m))
+    
     if index == 0:
         for i in range(int(C/cost[index])+1):  # C/ci
             arr = np.append(arr, prob(p[index], i)*np.exp(-__lambda__*i*weight[index]))
@@ -81,27 +74,17 @@ max_lambda = 0
 max_dictionary = {}
 global_wm = 0
 
-lambda_array = np.array([])
 wm_array = np.array([])
-lambda_last = 0
-lambda_current = 0
-lambda_min = 0
-lambda_max = 0
-lambda_min_wm = 0
-lambda_max_wm = 0
-lambda_max_dict = {}
 __lambda__ = 0
 global_wm = 0
 global_dictionary = {}
-lambda_min_dict = {}
 lambda_exceeded_status = False # if true then it exceeded from >0 to <0
-for i in np.arange(0.00001, 0.0005, 0.00001):
+lambda_max_dict = {}
+for i in np.arange(0.00001, 0.001, 0.00001):
     
-    lambda_last = __lambda__ 
-    print(bool(global_wm - W <= 0))
     __lambda__ = i
-    lambda_current = __lambda__
     print("lambda=", __lambda__)
+
     m = np.array([], dtype=int)
     arr_global = np.array([], dtype=int)
     C = C_global
@@ -110,67 +93,47 @@ for i in np.arange(0.00001, 0.0005, 0.00001):
 
     lambda_min_dict = copy.deepcopy(lambda_max_dict)
     lambda_max_dict = copy.deepcopy(global_dictionary)
-    answer = 1
-    # calculating the answer
-    for i in range(N):
-        answer = answer * prob(p[i],  global_dictionary["m"][i]) * np.exp(-__lambda__*global_dictionary["m"][i]*weight[i])
-
-
-    
-
-
-
-    print("m", global_dictionary["m"])
-    print("wm=", global_wm) 
-    print("cm", np.dot(global_dictionary["m"], cost))
-    print("the probability of successful operation is", answer*np.exp(__lambda__*np.dot(weight, global_dictionary["m"]))) 
-
-
 
     if boolean:
         boolean = False
-        max_probability = answer*np.exp(__lambda__*np.dot(weight, global_dictionary["m"])) 
+        max_dictionary = global_dictionary.copy()
+        max_probability = compute_global_prob(max_dictionary)["prob"]
+        max_lambda = __lambda__
+    if compute_global_prob(global_dictionary)["prob"] > max_probability:
         max_lambda = __lambda__
         max_dictionary = global_dictionary.copy()
-        print()
+        max_probability = compute_global_prob(max_dictionary)["prob"]
 
-    if (answer*np.exp(__lambda__*np.dot(weight, global_dictionary["m"]))) > max_probability:
-        max_lambda = __lambda__
-        max_dictionary = global_dictionary.copy()
-        max_probability = answer*np.exp(__lambda__*np.dot(weight, global_dictionary["m"])) 
-    print("max prob in each iteeration", max_probability)
-    
-    # wm_array = np.append(wm_array,global_wm)
-    # lambda_array = np.append(lambda_array,__lambda__) 
+    print("max_probability:", max_probability) 
     global_wm = np.dot(global_dictionary["m"], weight)
-
+'''
     if bool(global_wm - W <= 0) == True:
         break
-
+'''
 elapsed_time = time.time()-start_time
 answer = 1  #  final func 
 # calculating the answer
-for i in range(N):
-    answer = answer * prob(p[i],  max_dictionary["m"][i]) * np.exp(-max_lambda*max_dictionary["m"][i]*weight[i])
-
 print("\n\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
-print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
-print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
-max_lambda = 0.0008
 print("wm", np.dot(weight, max_dictionary["m"]))
 print("cm", np.dot(cost, max_dictionary["m"]))
-print("the max probability of successful operation is", answer*np.exp(max_lambda*np.dot(weight, max_dictionary["m"])))
+# print("the max probability of successful operation is", answer*np.exp(max_lambda*np.dot(weight, max_dictionary["m"])))
 print("max m", max_dictionary["m"])
 print("max_lambda=", max_lambda)
 print("\n\ntime elapsed for the program in ms ", elapsed_time*1000)
-print(lambda_array, wm_array)
 
-print("lambda_current=", lambda_current, "lambda_last=", lambda_last)
-print("lambda_min_wm=", lambda_min_wm, "lambda_max_wm", lambda_max_wm)
+print("lambda_current=", lambda_min_dict["lambda"], "lambda_last=", lambda_min_dict["m"])
+print("lambda_min_wm=", lambda_max_dict["lambda"], "lambda_max_wm", lambda_max_dict["m"])
+print(wm_array)
 # plt.scatter(wm_array, lambda_array, s=1)
 # plt.show()
 print("lambda_min_dict=", lambda_min_dict)
-
 print("lambda_max_dict=", lambda_max_dict)
-print(compute_global_prob(lambda_min_dict))
-print(compute_global_prob(lambda_max_dict))
+print()
+print("global_prob min:",compute_global_prob(lambda_min_dict))
+print("global_prob_max:",compute_global_prob(lambda_max_dict))
+
+
+
+
+
+
