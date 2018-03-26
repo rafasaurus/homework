e05eda6d1c14d18860768ead25b75f85e67b89eb
Rafael <rafa.grigorian@gmail.com>
2018-03-27 01:21:51 +0400

    cleared

--- operationsResearch/rreliable.py
+++ operationsResearch/rreliable.py
@@ -1,6 +1,6 @@
+#/usr/bin/python
 import numpy as np
 import time
-import matplotlib.pyplot as plt
 import copy
 
 l = [0.001, 0.0008, 0., 0.0001, 0.00001, 0.00000001, 0.1, 0.01]
@@ -23,6 +23,7 @@ index = N
 C = 130
 W = 120
 
+# *************************************************
 
 def prob(p, m):
     return 1-pow(1-p, 1+m)
@@ -52,32 +53,30 @@ def func(index, W, m, __lambda__):
     if index == 0:
         for i in range(int((W)/weight[index])+1):  # W/wi
             arr = np.append(arr, prob(p[index], i)*np.exp(-__lambda__*i*cost[index]))
+
         m = np.append(m, np.argmax(arr))  # arr comes with none so you should continue in line 72
         dictionary = {
-                "arr_max": np.max(arr),  # F(index)(C) 
-                "m": m, 
-                "lambda": __lambda__
-                }
-        # print(str(dictionary["m"]) + str(dictionary["arr_max"])+" "+str(compute_global_prob(dictionary)["cm"]))
-
+                        "arr_max": np.max(arr),  # F(index)(C) 
+                        "m": m, 
+                        "lambda": __lambda__
+                     }
         return dictionary
     else:
         for i in range(int((W)/weight[index]+1)):  # mj = C/cj # +1 ================================================================
             m = np.append(m, i)
-
             dictionary = func(index-1, W-i*weight[index], m, __lambda__)
             arr = np.append(arr, prob(p[index], i) * np.exp(-__lambda__*i*cost[index])*dictionary["arr_max"])
-
             m = m[:-1]
             m_arr = np.append(m_arr, dictionary)
 
-        dictionary = {"arr_max": np.max(arr),
-                      "m": m_arr[np.argmax(arr)]["m"],#  
-                      "lambda": __lambda__}
+        dictionary = {
+                        "arr_max": np.max(arr),
+                        "m": m_arr[np.argmax(arr)]["m"],#  
+                        "lambda": __lambda__
+                     }
         return dictionary
 
 boolean = True
-
 global_dictionary = {}
 m = np.array([], dtype=int)
 
@@ -85,7 +84,6 @@ for __lambda__ in np.arange(0.0001, 0.0004 , 0.00001):  # for my problem
     start_time = time.time()
     print("-----------------------------------------------------------------------")
     global_dictionary = func(index-1, W, m, __lambda__)  ######
-    # print("dict:", global_dictionary)
     print("global:", compute_global_prob(global_dictionary))        
     boolean = True
     elapsed_time = time.time()-start_time
