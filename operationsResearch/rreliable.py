6add104b76c0867c56cc458ec01ca39968d227a6
Rafael <rafa.grigorian@gmail.com>
2018-03-27 12:28:05 +0400

    left edge stuff

--- operationsResearch/rreliable.py
+++ operationsResearch/rreliable.py
@@ -24,6 +24,33 @@ C = 130
 W = 120
 
 # *************************************************
+def fixed_left_edge_chord(leftLambda, __lambda__, iterations):  #fixed with left edge chord method "quasi-Newton method" 167 Simonyan
+    for o in range(iterations):
+        start_time = time.time()
+        print("-----------------------------------------------------------------------")
+        # global_dictionary = func(index-1, W, m, __lambda__)  ######
+        # print("global:", compute_global_prob(global_dictionary))        
+        # boolean = True
+         
+
+        lambdaf_dict = compute_global_prob(func(index-1, W, m, __lambda__))
+        print("lambda:", __lambda__ , "\ndict", lambdaf_dict) 
+        lambdaf = lambdaf_dict["cm"]-C
+        boolean = True
+        
+        if lambdaf <= (min(cost)):
+            return lambdaf_dict
+            
+
+        lambdaf_left_dict = compute_global_prob(func(index-1, W, m, leftLambda))
+        lambdaf_left = lambdaf_left_dict["cm"]-C
+        boolean = True
+
+        __lambda__ = __lambda__ - ((leftLambda - __lambda__) * lambdaf) / (lambdaf_left - lambdaf) #####
+        print("next labmda:", __lambda__)
+        elapsed_time = time.time()-start_time
+        print("time elapsed for the program in ms ", elapsed_time*1000)
+
 
 def prob(p, m):
     return 1-pow(1-p, 1+m)
@@ -79,7 +106,7 @@ def func(index, W, m, __lambda__):
 boolean = True
 global_dictionary = {}
 m = np.array([], dtype=int)
-
+'''
 for __lambda__ in np.arange(0.0001, 0.0004 , 0.00001):  # for my problem
     start_time = time.time()
     print("-----------------------------------------------------------------------")
@@ -88,8 +115,13 @@ for __lambda__ in np.arange(0.0001, 0.0004 , 0.00001):  # for my problem
     boolean = True
     elapsed_time = time.time()-start_time
     print("time elapsed for the program in ms ", elapsed_time*1000)
+'''
+best_dict = fixed_left_edge_chord(leftLambda = 0.00001, __lambda__ = 0.000015, iterations = 10)
 
-print("W:", W)
-print("C:", C)
+print("\n\n\n\n***************** found the best labmda dict **********************\n\n")
+print("the best dict: ", best_dict)
+
+print("\n\n\nW:", W)
+print("C:", C, " +/- " + str(min(cost)))
 print("cost:", cost)
 print("weight:", weight)
