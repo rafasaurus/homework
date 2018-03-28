f592f285a60397829e043c2812f0439cfe31ce03
Rafael <rafa.grigorian@gmail.com>
2018-03-29 00:03:46 +0400

    end

--- operationsResearch/rreliable.py
+++ operationsResearch/rreliable.py
@@ -2,7 +2,7 @@
 import numpy as np
 import time
 import copy
-
+import matplotlib.pyplot as plt
 l = [0.001, 0.0008, 0., 0.0001, 0.00001, 0.00000001, 0.1, 0.01]
 
 # ****************** input data *******************
@@ -25,34 +25,49 @@ W = 120
 
 # *************************************************
 def fixed_left_edge_chord(leftLambda, __lambda__, iterations):  #fixed with left edge chord method "quasi-Newton method" 167 Simonyan
-    for o in range(iterations):
+    global boolean
+    global plot_prob_dict
+    global plot_lambda_dict
+    global plot_cm
+    local_boolean = False
+    max_probability = 0
+    for __iter__ in range(iterations):
         start_time = time.time()
         print("-----------------------------------------------------------------------")
         # global_dictionary = func(index-1, W, m, __lambda__)  ######
         # print("global:", compute_global_prob(global_dictionary))        
         # boolean = True
          
-
+        print("iteration number:", __iter__)
         lambdaf_dict = compute_global_prob(func(index-1, W, m, __lambda__))
         print("lambda:", __lambda__ , "\ndict", lambdaf_dict) 
         # lambdaf_dict["lambda"] = round(lambdaf_dict["lambda"],5)
         lambdaf = lambdaf_dict["cm"]-C
+        plot_cm = np.append(plot_cm, lambdaf)
         boolean = True
-        
-        # if lambdaf <= (min(cost)):
-        #     return lambdaf_dict
-            
 
+
+
+        # plot_lambda_dict = np.append(plot_lambda_dict, __lambda__)
+        plot_lambda_dict = np.append(plot_lambda_dict, __iter__)
+        plot_prob_dict = np.append(plot_prob_dict, lambdaf_dict["prob"])
         lambdaf_left_dict = compute_global_prob(func(index-1, W, m, leftLambda))
         # lambdaf_left_dict["lambda"] = round(lambdaf_dict["lambda"],5)
         lambdaf_left = lambdaf_left_dict["cm"]-C
         boolean = True
-
+        
+        last_labmda = __lambda__
         __lambda__ = __lambda__ - ((leftLambda - __lambda__) * lambdaf) / (lambdaf_left - lambdaf) #####
         print("next labmda:", __lambda__)
         elapsed_time = time.time()-start_time
         print("time elapsed for the program in ms ", elapsed_time*1000)
 
+        # checks if the probability is maximum at C-sum(cm) condition, not that important
+        if abs(lambdaf) <= (min(cost)):
+            local_boolean = True
+        if last_labmda > __lambda__ and local_boolean:
+            print("0000000000000000", lambdaf_dict)
+            return lambdaf_dict
 
 def prob(p, m):
     return 1-pow(1-p, 1+m)
@@ -108,18 +123,24 @@ def func(index, W, m, __lambda__):
 boolean = True
 global_dictionary = {}
 m = np.array([], dtype=int)
+
+plot_prob_dict = np.array([])
+plot_lambda_dict = np.array([])
+plot_cm = np.array([])
 '''
-for __lambda__ in np.arange(0.0001, 0.0004 , 0.00001):  # for my problem
+for __lambda__ in np.arange(0.00015, 0.00027 , 0.00001):  # for my problem
     start_time = time.time()
     print("-----------------------------------------------------------------------")
     global_dictionary = func(index-1, W, m, __lambda__)  ######
     print("global:", compute_global_prob(global_dictionary))        
+    plot_lambda_dict = np.append(plot_lambda_dict, __lambda__)
+    plot_prob_dict = np.append(plot_prob_dict, compute_global_prob(global_dictionary))
     boolean = True
     elapsed_time = time.time()-start_time
     print("time elapsed for the program in ms ", elapsed_time*1000)
 '''
-best_dict = fixed_left_edge_chord(leftLambda = 0.00001, __lambda__ = 0.000015, iterations = 100)
-best_dict["lambda"] = round(best_dict["lambda"], 5)
+######### best_dict["lambda"] = round(best_dict["lambda"], 5)
+best_dict = fixed_left_edge_chord(leftLambda = 0.0001, __lambda__ = 0.0002, iterations = 15)
 print("\n\n\n\n***************** found the best labmda dict **********************\n\n")
 print("the best dict: ", best_dict)
 
@@ -127,3 +148,15 @@ print("\n\n\nW:", W)
 print("C:", C, " +/- " + str(min(cost)))
 print("cost:", cost)
 print("weight:", weight)
+plt.subplot(122)
+plt.title("prob - lambda")
+plt.xlabel('labmda')
+plt.ylabel('probability')
+plt.scatter(plot_lambda_dict, plot_prob_dict)
+
+plt.subplot(121)
+plt.title("cm")
+plt.ylabel('C-cm')
+plt.xlabel('lambda')
+plt.scatter(plot_lambda_dict, plot_cm)
+plt.show()
