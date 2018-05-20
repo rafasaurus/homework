a38780d459aeda803e15aa20a1f9e4f4c43c6fad
rafasaurus <rafa.grigorian@gmail.com>
2018-05-21 01:05:59 +0400

    april 23 night

--- operationsResearch/reliability_problem.py
+++ operationsResearch/reliability_problem.py
@@ -2,35 +2,37 @@
 import numpy as np
 import time
 import copy
-import matplotlib.pyplot as plt
 l = [0.001, 0.0008, 0., 0.0001, 0.00001, 0.00000001, 0.1, 0.01]
 
-# ****************** input data *******************
-
+#****************** input data *******************
+# __lambda__ = 0.0008
 # p = np.array([0.9, 0.75, 0.65, 0.8, 0.85], dtype=float)
 # weight = np.array([5, 4, 9, 7, 7], dtype=int)
 # cost = np.array([8, 9, 6, 7, 8], dtype=int)
 # N = 5
-# index = N
+# index = 0
 # C = 104
 # W = 100
-'''
+
+__lambda__ = 0.00022
 p = np.array([0.9, 0.85, 0.88, 0.75, 0.9, 0.8], dtype=float)
 weight = np.array([4, 8, 7, 3, 5, 3], dtype=int)
 cost = np.array([4, 6, 12, 10, 5, 8], dtype=int)
 N = 6
-index = N
+index = 0
 C = 130
 W = 120
-'''
-p = np.array([0.9, 0.85, 0.88, 0.75, 0.9, 0.8, 0.75], dtype=float)
-weight = np.array([4, 8, 7, 3, 5, 3, 6], dtype=int)
-cost = np.array([4, 6, 12, 10, 5, 8, 10], dtype=int)
-N = 7
-index = N
-C = 120
-W = 100
+
+#__lambda__ = 0.00002
+#p = np.array([0.9, 0.85, 0.88, 0.75, 0.9, 0.8, 0.75], dtype=float)
+#weight = np.array([4, 8, 7, 3, 5, 3, 6], dtype=int)
+#cost = np.array([4, 6, 12, 10, 5, 8, 10], dtype=int)
+#index = 0
+#N = 7
+#C = 120
+#W = 100
 # *************************************************
+'''
 def fixed_left_edge_chord(leftLambda, __lambda__, iterations):  #fixed with left edge chord method "quasi-Newton method" 167 Simonyan
     """
     finds the best lambda, and the best dictionary for dynamic programming problem,
@@ -89,9 +91,7 @@ def fixed_left_edge_chord(leftLambda, __lambda__, iterations):  #fixed with left
             
             return lambdaf_dict
 
-
-def prob(p, m):
-    return 1-pow(1-p, 1+m)
+'''
 
 
 def compute_global_prob(dictionary):
@@ -110,8 +110,10 @@ def compute_global_prob(dictionary):
                     "lambda": dictionary["lambda"]}
     return return_dict 
 
+def prob(p, m):
+    return 1-pow(1-p, 1+m)
 
-def func(index, W, m, __lambda__):
+def func(index, W, m, __lambda__, arr_max):
     """
     Bellman recurrent function
     """
@@ -120,52 +122,50 @@ def func(index, W, m, __lambda__):
     global debug_index
     if index == 0:
         for i in range(int((W)/weight[index])+1):  # W/wi
-            arr = np.append(arr, prob(p[index], i)*np.exp(-__lambda__*i*cost[index]))
 
+            arr = np.append(arr, prob(p[index], i)*np.exp(-__lambda__*i*cost[index]))
+            print((prob(p[index],i)),"__", np.exp(-__lambda__*i*cost[index]),"=",prob(p[index], i)*np.exp(-__lambda__*i*cost[index]))
+        print("\n\t\tTABLE:", index)
+        print(arr.T)
+        print("max_table:", np.max(arr))
+        print("max_m:", np.argmax(arr))
         m = np.append(m, np.argmax(arr))  # arr comes with none so you should continue in line 72
-        dictionary = {
-                        "arr_max": np.max(arr),  # F(index)(C) 
-                        "m": m, 
-                        "lambda": __lambda__
-                     }
+        arr_max = np.max(arr)
+        dictionary = func(index+1, W-weight[index]*np.argmax(arr), m, __lambda__, arr_max)
         return dictionary
     else:
-        for i in range(int((W)/weight[index]+1)):  # mj = C/cj # +1 ================================================================
-            m = np.append(m, i)
-            dictionary = func(index-1, W-i*weight[index], m, __lambda__)
-            arr = np.append(arr, prob(p[index], i) * np.exp(-__lambda__*i*cost[index])*dictionary["arr_max"])
-            m = m[:-1]
-            m_arr = np.append(m_arr, dictionary)
+        for i in range(int((W)/weight[index]+1)):  # mj = C/cj #=
+            #m = np.append(m, i)
+            #dictionary = func(index-1, W-i*weight[index], m, __lambda__)
+            arr = np.append(arr, prob(p[index], i) * np.exp(-__lambda__*i*cost[index])*arr_max)
+            print((prob(p[index],i)),"__", np.exp(-__lambda__*i*cost[index])*arr_max,"=",prob(p[index], i) * np.exp(-__lambda__*i*cost[index])*arr_max)
 
+            #m = m[:-1]
+            #m_arr = np.append(m_arr, dictionary)
+        print("\n\t\tTABLE:", index-1, "__", W)
+        print(arr)
+        print("max_table:", np.max(arr))
+        print("max_m:", np.argmax(arr))
+        arr_max = np.max(arr)
+        m=np.append(m, np.argmax(arr))
         dictionary = {
                         "arr_max": np.max(arr),
-                        "m": m_arr[np.argmax(arr)]["m"],#  
+                        "m": m,  
                         "lambda": __lambda__
                      }
-        return dictionary
+        if index+1 >= N:
+            return dictionary
+        else:
+            return func(index+1, W-weight[index]*np.argmax(arr), m, __lambda__, arr_max)
 
 boolean = True
 global_dictionary = {}
 m = np.array([], dtype=int)
 
-plot_prob_arr = np.array([])
-plot_index_arr = np.array([])
-plot_lambda_arr  = np.array([])
-plot_cm = np.array([])
-'''
-for __lambda__ in np.arange(0.00015, 0.00027 , 0.00001):  # for my problem
-    start_time = time.time()
-    print("-----------------------------------------------------------------------")
-    global_dictionary = func(index-1, W, m, __lambda__)  ######
-    print("global:", compute_global_prob(global_dictionary))        
-    plot_lambda_dict = np.append(plot_lambda_dict, __lambda__)
-    plot_prob_dict = np.append(plot_prob_dict, compute_global_prob(global_dictionary))
-    boolean = True
-    elapsed_time = time.time()-start_time
-    print("time elapsed for the program in ms ", elapsed_time*1000)
-'''
-######### best_dict["lambda"] = round(best_dict["lambda"], 5)
-best_dict = fixed_left_edge_chord(leftLambda = 0.0001, __lambda__ = 0.00007, iterations = 15)
+
+arr_max=0 
+
+best_dict = compute_global_prob(func(index, W, m, __lambda__, arr_max))
 print("\n\n\n\n***************** found the best labmda dict ******************\n\n")
 print("the best dict: ", best_dict)
 
@@ -173,18 +173,4 @@ print("\n\n\nW:", W)
 print("C:", C, " +/- " + str(min(cost)))
 print("cost:", cost)
 print("weight:", weight)
-
-plt.subplot(122)
-plt.title("probability dependence with iterations of lambda")
-plt.xlabel('iterations')
-plt.xticks(plot_index_arr, np.round(plot_lambda_arr,6))
-plt.ylabel('probability')
-plt.plot(plot_index_arr, plot_prob_arr)
-
-plt.subplot(121)
-plt.title("C-cm dependence with iterations of lambda")
-plt.xticks(plot_index_arr, np.round(plot_lambda_arr,6))
-plt.xlabel('iterations')
-plt.ylabel('C-cm')
-plt.plot(plot_index_arr, plot_cm)
-plt.show()
+print("p:", p)
