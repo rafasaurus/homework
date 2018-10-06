8c1a85b8b4945d953f8558a8d610892858509e0e
rafasaurus <rafa.grigorian@gmail.com>
2018-10-07 00:30:00 +0400

    gitignore and ipynb in bank

--- bankSystems/bankAutomatedControl/kursain.py
+++ bankSystems/bankAutomatedControl/kursain.py
@@ -14,3 +14,18 @@ data = pd.read_csv('bank.csv', header=0)
 data = data.dropna()
 print(data.shape)
 print(list(data.columns))
+data2 = pd.get_dummies(data, columns =['job', 'marital', 'default', 'housing', 'loan', 'poutcome'])
+data2.drop(data2.columns[[12, 16, 18, 21, 24]], axis=1, inplace=True)
+sns.heatmap(data2.corr())
+# plt.show()
+X = data2.iloc[:,1:]
+y = data2.iloc[:,0]
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+classifier = LogisticRegression(random_state=0)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+from sklearn.metrics import confusion_matrix
+confusion_matrix = confusion_matrix(y_test, y_pred)
+print(confusion_matrix)
