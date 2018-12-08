24f75e00497bfbbd05c4b7a17813853a46178e70
rafasaurus <rafa.grigorian@gmail.com>
2018-12-08 14:28:25 +0400

    end of lab5

--- bank_systems/finance_bank_systems/4grade/bank_lab5.py
+++ bank_systems/finance_bank_systems/4grade/bank_lab5.py
@@ -19,40 +19,73 @@ def pureSalary(greedySalary_):
 
     return greedySalary_
 
+def pureHolidayFee():
+    holidayTaxJune, holidayPensionsJune, overallDailyFeeJune = getholidayTaxAndPensions(11, 300000) 
+    holidayTaxJuly, holidayPensionsJuly, overallDailyFeeJuly = getholidayTaxAndPensions(9, 300000) 
+    overallHolidayFee = overallDailyFeeJune + overallDailyFeeJuly
+    return overallHolidayFee - holidayTaxJune - holidayTaxJuly - holidayPensionsJune - holidayPensionsJuly
 
-def holidaySalaryDailyFee(days, greedySalary):
+def getholidayTaxAndPensions(days, greedySalary):
     # ''' returns եկամտահարկ, կենսաթոշակ, military stuff '''
-    # hevelavhchar F
-    # oravardz
-    premium = 20000
-    commitmentForWeekends = 10000
-    holidaySalary = 5000
-    surcharge = premium + commitmentForWeekends + holidaySalary
-    holydayDailyFee = (surcharge/12 + greedySalary)/21
-    overallDailyFee = days * holydayDailyFee
 
-    # եկամտահարկ
-    greedySalary -= greedySalary / 21 * (21 - days)
-    greedySalary += overallDailyFee
-    if (greedySalary <= 150000):
-        overallDailyFee -= greedySalary*0.23
-    elif (overallDailyFee <=2000000):
-        overallDailyFee -= 150000*0.23 + (greedySalary - 150000*0.23)*0.28
+    premium = 200000
+    commitmentForWeekends = 30000
+    holidaySalary = 60000
+    pureSalary = 200000 # for 15 days that had been worked
+    surcharge = premium + commitmentForWeekends + holidaySalary # հավելավճար F
+    holidayDailyFee = (surcharge/12 + greedySalary)/21
+    overallDailyFee = days * holidayDailyFee
+    print("overallDailyFee: ", overallDailyFee)
+
+    # ____________________________________
+    # եկամտահարկ income tax
+    # աճողական գումար եկամտահարկ հունիս ամսվա
+    pureSalary_ = copy.deepcopy(pureSalary)
+    pureSalary_ += overallDailyFee
+    pureSalary += overallDailyFee
+    if (pureSalary_ <= 150000):
+        pureSalary_ -= pureSalary*0.23
+    elif (pureSalary_ <=2000000):
+        pureSalary_ -= 150000*0.23 + (pureSalary - 150000)*0.28
     else:
-        overallDailyFee -= 150000*0.23 + (1850000 - 150000*0.23)*0.28 + (greedySalary - (1850000 - 150000*0.23)*0.28)*0.36
+        pureSalary_ -= 2000000*0.28 + (pureSalary - 2000000)*0.36
+    # կենսաթոշակ
+    if (pureSalary * 0.025 <= 12500):
+        pensionsForMonth = pureSalary * 0.025
+    else:
+        pensionsForMonth -= 12500
+    # print("pensionsForMonth: ", pensionsForMonth)
+    incomeTaxForTheMonth = copy.deepcopy(pureSalary - pureSalary_)
+    # print("incomeTaxForTheMonth: ", incomeTaxForTheMonth)
+    # ____________________________________
+
+    # ************************************
+    pureSalary -= overallDailyFee
+    # եկամտահարկ income tax
+    incomTaxForDaysWorked = 0
+    if (pureSalary <= 150000):
+        incomTaxForDaysWorked = pureSalary*0.23
+    elif (pureSalary_ <=2000000):
+        incomTaxForDaysWorked = 150000*0.23 + (pureSalary - 150000)*0.28
+    else:
+        incomTaxForDaysWorked = 2000000*0.28 + (pureSalary - 2000000)*0.36
 
     # կենսաթոշակ
-    if (greedySalary * 0.025 <= 12500):
-        overallDailyFee -= greedySalary * 0.025
+    if (pureSalary * 0.025 <= 12500):
+        pensionsForDays = pureSalary * 0.025
     else:
-        overallDailyFee -= 12500
-    # millitary stuff
-    overallDailyFee -= 1000
-    return overallDailyFee
+        pensionsForDays = 12500
+    print("pensionsForDays: ", pensionsForDays)
+    # ************************************
+
+    # արձակուրդի եկամտահարկ, և կենսաթոշակ 
+    holidayTax = incomeTaxForTheMonth - incomTaxForDaysWorked
+    holidayPensions = pensionsForMonth - pensionsForDays
+    return holidayTax, holidayPensions, overallDailyFee
 
 
-salary = 5000000
-days = 10
+salary = 300000
+days = 11
 print("Salary: ", salary)
 print("pure Salary: ", pureSalary(salary))
-print("holidarySalaryDailyFee for ", days, " days: ", holidaySalaryDailyFee(days, salary))
+print("pureHolidayFee: ", pureHolidayFee())
